65694b9d4fffa4e57de7223172083a865244ab9c
YehorIshchuk <159074080+YehorIshchuk@users.noreply.github.com>
2024-03-05 01:50:47 +0200

    Add files via upload
    Add a Menu header file

--- Menu.h
+++ Menu.h
@@ -1,26 +1,34 @@
 #include <iostream>
 using namespace std;
 
-
-
 class Menu {
 private:
     int price;
     string nameofdish;
+    int quantityofdrinks;
 
 public:
-    
-    Menu() : Menu(55, "Carbonara") {}
-
-    
-    Menu(int p, string name) : price(p), nameofdish(name) {}
-
+    Menu() : price(0), nameofdish("Carbonara") {
+    }
     ~Menu() {
         cout << "Menu object destroyed" << endl;
     }
+     void PrintQuantity() const{
+
+    cout <<"Quantity of Drinks:" << this -> quantityofdrinks<<endl;
+}
+    void SetQuantity(int newquantityofdrinks){
+this -> quantityofdrinks = newquantityofdrinks;
+
+    }
 
     void Print() {
-        cout << "Price: " << price << endl;
+        cout << "Price: " <<  price << endl;
         cout << "Name of Dish: " << nameofdish << endl;
+    }
+    void SetPrice(int price)
+    {
+        this -> price = price;
+        cout << "Price:" << this -> price << endl;
     }
 };
