2ce2c4c65338a1659aa46faf7c53426d99c4e453
YehorIshchuk <159074080+YehorIshchuk@users.noreply.github.com>
2024-03-05 01:49:09 +0200

    Add files via upload
    Add a class Staf Header file

--- Staff.h
+++ Staff.h
@@ -1,20 +1,31 @@
-
-#include <iostream>
-using namespace std;
-
-
-
 class Staff {
 public:
     int freecars;
     int priceofdelivery;
+    static int orders;
+
+    static int GetCount() {
+        return orders;
+    }
+
+    static void OrdersCount() {
+        orders++;
+    }
 
     Staff() : freecars(0), priceofdelivery(0) {}
 
-    
-    Staff(int valuex, bool boolean) : priceofdelivery(valuex), freecars(boolean ? 1 : 0) {}
+    Staff(int valuex, bool boolean) : priceofdelivery(valuex) {
+        if (boolean) {
+            freecars = 1;
+        } else {
+            cout << "No cars available" << endl;
+        }
+    }
 
-    Staff(int valuex) : Staff(valuex, true) {} 
+    Staff(int valuex) : priceofdelivery(valuex), freecars(1) {}
+
+    
+    Staff(const Staff& other) : freecars(other.freecars), priceofdelivery(other.priceofdelivery) {}
 
     ~Staff() {
         cout << "Staff object destroyed" << endl;
