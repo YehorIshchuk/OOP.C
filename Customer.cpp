a9c832f8f8227650b1512e3671801b8d6a7c86be
YehorIshchuk <159074080+YehorIshchuk@users.noreply.github.com>
2024-03-05 01:50:22 +0200

    Add files via upload
    Add a Customer cpp file

--- Customer.cpp
+++ Customer.cpp
@@ -1,9 +1,34 @@
-#include "Customer.h"
+#include <iostream>
+#include"Customer.h"
 using namespace std;
 
+Customer(int value) : numberoforders(5), profit(1079) {
+        data = new int(value);
+    }
 
-Customer :: Customer() : numberoforders(5), profit(1079) {}
+    
+    Customer(Customer && other)  {
+        cout << "Move constructor was called" << endl;
+        data = other.data;
+        other.data = nullptr;
+    }
 
+    
     ~Customer() {
+        delete data;
         cout << "Customer object destroyed" << endl;
+    }
+
+    
+    Customer operator-() const {
+        Customer result(*this); 
+        *result.data = -*result.data; 
+        return result;
+    }
+
+    
+    Customer operator+(const Customer& other) const {
+        Customer result(0);
+        *result.data = *data + *other.data;
+        return result;
     }
