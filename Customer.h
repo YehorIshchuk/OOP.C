c54d87b090655b5b5c81eb7abaa8a0825ffb2b92
YehorIshchuk <159074080+YehorIshchuk@users.noreply.github.com>
2024-03-05 01:50:01 +0200

    Add files via upload
    add a class Customer header file

--- Customer.h
+++ Customer.h
@@ -1,22 +1,52 @@
-
 #include <iostream>
 using namespace std;
+
 
 class Customer {
 private:
     int numberoforders;
     int profit;
+    int* data;
+    int age;
+    string name;
 
 public:
     
-    Customer() : numberoforders(5), profit(1079) {}
+    Customer(int value) : numberoforders(5), profit(1079) {
+        data = new int(value);
+    }
 
+    
+    Customer(Customer && other)  {
+        cout << "Move constructor was called" << endl;
+        data = other.data;
+        other.data = nullptr;
+    }
+
+    
     ~Customer() {
+        delete data;
         cout << "Customer object destroyed" << endl;
     }
 
-    void Print() {
+    
+    Customer operator-() const {
+        Customer result(*this); 
+        *result.data = -*result.data; 
+        return result;
+    }
+
+    
+    Customer operator+(const Customer& other) const {
+        Customer result(0);
+        *result.data = *data + *other.data;
+        return result;
+    }
+
+    
+    void Print() const {
         cout << "Number of Orders: " << numberoforders << endl;
         cout << "Profit: " << profit << endl;
+        cout << "Data value: " << *data << endl;
     }
 };
