b2b3b1df79929c39688b6e7c8296c34056af1332
YehorIshchuk <159074080+YehorIshchuk@users.noreply.github.com>
2024-03-04 20:57:04 +0200

    Add files via upload
    Add a class Customer header file

---
+++ Customer.h
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+
+class Customer {
+private:
+    int numberoforders;
+    int profit;
+
+public:
+    
+    Customer() : numberoforders(5), profit(1079) {}
+
+    ~Customer() {
+        cout << "Customer object destroyed" << endl;
+    }
+
+    void Print() {
+        cout << "Number of Orders: " << numberoforders << endl;
+        cout << "Profit: " << profit << endl;
+    }
+};
